4a0cf019b194fd9d1f51dad99d978064d9c56f13
fernando <fernando.montero.perez@gmail.com>
2019-02-15 14:15:17 +0000

    Fixed index page

--- com4513_lab1/main.py
+++ com4513_lab1/main.py
@@ -5,6 +5,8 @@ import numpy as np
 
 directory = os.path.join("c:\\", "path")
 
+def sigmoid (x): return 1/(1 + np.exp(-x))      # activation function
+def sigmoid_(x): return x * (1 - x)        
 
 def word_count(str):
     words = str.split()
@@ -29,14 +31,17 @@ def process_training_data():
                     bag_of_words.update(c.Counter(counted_words))
                 # print(file)
                 count += 1
+        
     return bag_of_words
+
 
 def calculate_weights(training_set):
     weighting = dict()
+    w = 0;
     for key, count in training_set.items():
         if key not in weighting:
             weighting[key] = 0
-        pred_y = key*count
+        pred_y = w * count
         if count != pred_y:
             if pred_y == 1:
                 weighting[key] += training_set[key]
@@ -44,9 +49,10 @@ def calculate_weights(training_set):
                 weighting[key] -= training_set[key]
     return weighting
 
-
+def prediction():
+    return
 
 bag_o_words = process_training_data()
 calculated_weight = calculate_weights(bag_o_words)
 print(bag_o_words)
-print(calculated_weight)
+print(calculated_weights)
